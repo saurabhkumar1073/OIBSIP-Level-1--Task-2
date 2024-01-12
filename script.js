ad2d27682df348315eb6f712dccad2ae550a8102
unknown <saurabhmishra1073@gmail.com>
2024-01-12 22:03:51 +0530

    Second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const sidebar=document.querySelector(".sidebar");
+function showsidebar()  {
+      sidebar.style.right="0";
+      sidebar.style.display="flex";
+    }
+function hidesidebar(){
+    sidebar.style.right="-700px";
+}
